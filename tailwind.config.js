ed3e5511ca665612fa5513753165d5e21c0fe69f
Tonje Albertin Kristiansen <149260628+T0nj3@users.noreply.github.com>
2025-03-21 12:46:22 +0100

    big first commit, added all pages needed, including content

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}", "!./node_modules/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
